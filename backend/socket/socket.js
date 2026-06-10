063e7dfe3fc342fd89b6c76b29a7752795731475
Shivanipatil-4 <shivanishivapatil@gmail.com>
2026-06-10 19:23:25 +0530

    Update CORS origin for socket server

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -7,7 +7,7 @@ let app = express()
 const server=http.createServer(app)
 const io=new Server(server,{
     cors:{
-        origin:"http://localhost:5173"
+        origin:"https://real-time-chat-app-frontend-61vf.onrender.com"
     }
 })
  const userSocketMap ={}
